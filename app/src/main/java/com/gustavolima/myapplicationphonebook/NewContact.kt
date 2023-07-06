2aec681f34040f16493c4b2574a31a071f26533e
Gustavo Lima <gustavo.f12@hotmail.com>
2023-07-06 20:52:24 -0300

    New contact layout created.

---
+++ app/src/main/java/com/gustavolima/myapplicationphonebook/NewContact.kt
@@ -0,0 +1,11 @@
+package com.gustavolima.myapplicationphonebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewContact : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_contact)
+    }
+}
