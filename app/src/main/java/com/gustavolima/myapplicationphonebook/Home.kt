1994262a8f088a6b75de388266ea366d2f98d3e8
Gustavo Lima <gustavo.f12@hotmail.com>
2023-07-06 20:53:56 -0300

    View binding implemented in Home.

--- app/src/main/java/com/gustavolima/myapplicationphonebook/Home.kt
+++ app/src/main/java/com/gustavolima/myapplicationphonebook/Home.kt
@@ -2,10 +2,13 @@ package com.gustavolima.myapplicationphonebook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gustavolima.myapplicationphonebook.databinding.ActivityHomeBinding
 
 class Home : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
